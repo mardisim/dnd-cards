228810fc00e58439e38fc110e069f7c989d53ae4
Marcello di Simone <marcello.disimone@googlemail.com>
2024-12-03 11:43:16 +0100

    add missing interceptor

--- apps/dnd-cards/src/app/app.config.ts
+++ apps/dnd-cards/src/app/app.config.ts
@@ -2,13 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { JwtInterceptor } from '@dnd-cards/client/auth';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
     provideRouter(appRoutes),
+    provideAnimationsAsync(),
   ],
 };
