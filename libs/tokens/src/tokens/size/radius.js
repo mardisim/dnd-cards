91bd7b099d1ef54390a83df0a5dc10dcb984b6b0
Marcello di Simone <marcello.disimone@googlemail.com>
2024-12-08 23:29:50 +0100

    update tokens

---
+++ libs/tokens/src/tokens/size/radius.js
@@ -0,0 +1,12 @@
+module.exports = {
+  border: {
+    radius: {
+      small: { value: '0.1875', comment: 'small border radius', type: 'borderRadius' },
+      medium: { value: '0.25', comment: 'medium border radius', type: 'borderRadius' },
+      large: { value: '0.5', comment: 'large border radius', type: 'borderRadius' },
+      xl: { value: '1', comment: 'extra large border radius', type: 'borderRadius' },
+      circle: { value: '50%', comment: 'circle shared border radius', type: 'borderRadius' },
+      pill: { value: '9999px', comment: 'pill shaped border radius', type: 'borderRadius' },
+    },
+  },
+};
