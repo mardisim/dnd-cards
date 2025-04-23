69b538b8c01f4ce0331f2bfe120ce70509da9319
Marcello di Simone <marcello.disimone@googlemail.com>
2025-04-23 23:26:32 +0200

    add  http proxy

--- libs/client/spell/src/lib/spell.service.ts
+++ libs/client/spell/src/lib/spell.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { ISpellModel } from '@interfaces';
 import { Observable } from 'rxjs';
 
-const AUTH_API_URL = 'http://localhost:3000/api';
+const AUTH_API_URL = '/api/spell';
 
 @Injectable({
   providedIn: 'root',
@@ -12,10 +12,10 @@ export class SpellService {
   private http = inject(HttpClient);
 
   getAllSpells(): Observable<ISpellModel[]> {
-    return this.http.get<ISpellModel[]>(`${AUTH_API_URL}/spell`);
+    return this.http.get<ISpellModel[]>(`${AUTH_API_URL}`);
   }
 
   getSpell(id: number): Observable<ISpellModel> {
-    return this.http.get<ISpellModel>(`${AUTH_API_URL}/spell/${id}`);
+    return this.http.get<ISpellModel>(`${AUTH_API_URL}/${id}`);
   }
 }
